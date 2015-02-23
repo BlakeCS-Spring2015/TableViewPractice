a0a27c402e152c9d6be5834c72a929299810bb11
clnye15 <clnye15@TBSLTMBA652.local>
2015-02-23 12:52:21 -0600

    Changes
    The work we did in class

---
+++ TableViewPractice/NewTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewTableViewController.h
+//  TableViewPractice
+//
+//  Created by clnye15 on 2/23/15.
+//  Copyright (c) 2015 Carolyn Nye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewTableViewController : UITableViewController
+
+@end
